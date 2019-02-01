dd5e56a0e43b8c84560d6a03d8931ee164ef3036
Michael M <Schnark@users.noreply.github.com>
2019-02-01 10:09:40 +0100

    Fix service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@
 (function (worker) {
 "use strict";
 
-var VERSION = 'v1.5',
+var PREFIX = 'barcode-reader',
+	VERSION = '1.5',
 	FILES = [
 		'app.css',
 		'index.html',
@@ -20,7 +21,7 @@ var VERSION = 'v1.5',
 
 worker.addEventListener('install', function (e) {
 	e.waitUntil(
-		caches.open(VERSION).then(function (cache) {
+		caches.open(PREFIX + ':' + VERSION).then(function (cache) {
 			return cache.addAll(FILES);
 		})
 	);
@@ -30,7 +31,7 @@ worker.addEventListener('activate', function (e) {
 	e.waitUntil(
 		caches.keys().then(function (keys) {
 			return Promise.all(keys.map(function (key) {
-				if (key !== VERSION) {
+				if (key.indexOf(PREFIX + ':') === 0 && key !== PREFIX + ':' + VERSION) {
 					return caches.delete(key);
 				}
 			}));
